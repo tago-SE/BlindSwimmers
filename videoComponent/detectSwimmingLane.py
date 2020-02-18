c2c5690fa49c34a531d7b330d384955a6846ce7a
alnor <alnor@kth.se>
2020-02-18 14:30:56 +0100

    Rectangles/ROI
    Draws a rectangle over the swimming lane and a rectangle in between marking the region of interest representing the desirable location of detecting a swimmer if he/she swam past the external boundaries of the the swimming pool lane marked by the red color

--- videoComponent/detectSwimmingLane.py
+++ videoComponent/detectSwimmingLane.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy as np
 
+def smaller_heights(leftH,rightH):
+    if leftH<=rightH:
+        return leftH
+    else:
+        return rightH
+
 #Read raw image
 rawImage = cv2.imread('anotherpool.jpg',1)
 #Convert Image to HSV
@@ -21,9 +27,11 @@ contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NON
 cs = sorted(contours, key=cv2.contourArea) 
 if len(contours) != 0:
     cv2.drawContours(mask,contours,-1,255)
-    c = max(contours, key = cv2.contourArea)
+    #c = max(contours, key = cv2.contourArea)
+    #find coordinates of bounding rectangles
     x,y,w,h = cv2.boundingRect(cs[len(cs)-1])
     x2,y2,w2,h2 = cv2.boundingRect(cs[len(cs)-2])
+    #Identify which is the left vs the right lane
     if x > x2 :
         rightX,rightY,rightW,rightH = x,y,w,h
         leftX,leftY,leftW,leftH = x2,y2,w2,h2
@@ -34,8 +42,19 @@ if len(contours) != 0:
     #print(c)
     #draw the biggest contour (c) in green
    # cv2.rectangle(rawImage,(x,y),(x+w,y+h),(222,255,155),2)
-    cv2.rectangle(rawImage,(leftX,leftY),(leftX+leftW,leftY+leftH),(222,255,155),2)
-    cv2.rectangle(rawImage,(rightX,rightY),(rightX+rightW,rightX+rightH),(222,255,155),2)
+    print('leftY ', (leftY))
+    print('rightY ', (rightY))
+    humanCentreX = leftW+leftX
+    humanCentreY = rightY
+    print('human BOX X', humanCentreX, 'and Y ', humanCentreY)
+    humanCentreW = rightX-(leftX+leftW)
+    print('Height x', leftH, 'and Y', rightH)
+    print(rightX-(leftX+leftW))
+    humanCentreH = smaller_heights(leftH,rightH)
+    #Orange 255	 	165	 	0
+    cv2.rectangle(rawImage,(humanCentreX,humanCentreY),(humanCentreX+humanCentreW,humanCentreY+humanCentreH),(255,165,0),3)
+    cv2.rectangle(rawImage,(leftX,leftY),(leftX+leftW,leftY+leftH),(222,255,155),1)
+    cv2.rectangle(rawImage,(rightX,rightY),(rightX+rightW,rightX+rightH),(222,255,155),1)
     
 #Render the image
 cv2.imshow("Mask", mask)
