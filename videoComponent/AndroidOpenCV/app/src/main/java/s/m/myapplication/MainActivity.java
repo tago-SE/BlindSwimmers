566a2d708c4175b7ab176f4d685877ee0f16fbaa
Tiago Redaelli <tiago.redaelli@gmail.com>
2020-02-18 01:41:34 +0100

    Working OpenCV Android Template

---
+++ videoComponent/AndroidOpenCV/app/src/main/java/s/m/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package s.m.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV loader success.");
+        } else {
+            Log.e(TAG, "OpenCV loader failure.");
+        }
+
+        Log.w(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
