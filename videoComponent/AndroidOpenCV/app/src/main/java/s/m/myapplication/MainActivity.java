c5bf9626a87b53ce3c3489b3b05280a6bf192c47
Tiago Redaelli <tiago.redaelli@gmail.com>
2020-02-19 01:19:57 +0100

    Working OpenCV Camera Activity with rotation

--- videoComponent/AndroidOpenCV/app/src/main/java/s/m/myapplication/MainActivity.java
+++ videoComponent/AndroidOpenCV/app/src/main/java/s/m/myapplication/MainActivity.java
@@ -2,27 +2,41 @@ package s.m.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = MainActivity.class.getSimpleName();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        Log.w(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         if (OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV loader success.");
         } else {
             Log.e(TAG, "OpenCV loader failure.");
         }
-
-        Log.w(TAG, "onCreate");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, ShowCameraActivity.class);
+        startActivity(intent);
     }
 }
